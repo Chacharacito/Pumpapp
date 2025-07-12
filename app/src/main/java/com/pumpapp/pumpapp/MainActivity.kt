f69d2f19a84a911838484b510b8ffba9e657df9c
Josscoder <josscoder@hotmail.com>
2025-07-12 16:06:26 -0500

    se implemento el logo y se mejoro la interfaz principakl

--- app/src/main/java/com/pumpapp/pumpapp/MainActivity.kt
+++ app/src/main/java/com/pumpapp/pumpapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.pumpapp.pumpapp
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.ImageButton
+import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,8 +20,35 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val riego = findViewById<ImageButton>(R.id.ib_goteo)
 
-        riego.setImageResource(R.drawable.riego_por_goteo)
+        val sSistemaUnidades = findViewById<Spinner>(R.id.s_sistema_unidades)
+
+        val sistemas = arrayOf("Internacional", "Imperial")
+        sSistemaUnidades.adapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_1,
+            sistemas
+        )
+
+        val ibRiegoPorGoteo = findViewById<ImageButton>(R.id.ib_goteo)
+        ibRiegoPorGoteo.setImageResource(R.drawable.riego_por_goteo)
+
+        ibRiegoPorGoteo.setOnClickListener {
+            Toast.makeText(this, "Prueba 1", Toast.LENGTH_SHORT).show()
+        }
+
+        val ibRiegoPorAspersion = findViewById<ImageButton>(R.id.ib_aspersion)
+        ibRiegoPorAspersion.setImageResource(R.drawable.riego_por_aspersion)
+
+        ibRiegoPorAspersion.setOnClickListener {
+            Toast.makeText(this, "Prueba 2", Toast.LENGTH_SHORT).show()
+        }
+
+        val ibRiegoPorInundacion = findViewById<ImageButton>(R.id.ib_inundacion)
+        ibRiegoPorInundacion.setImageResource(R.drawable.riego_por_inundacion)
+
+        ibRiegoPorInundacion.setOnClickListener {
+            Toast.makeText(this, "Prueba 3", Toast.LENGTH_SHORT).show()
+        }
     }
 }
