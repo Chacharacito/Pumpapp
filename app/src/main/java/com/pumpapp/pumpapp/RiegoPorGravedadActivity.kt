96341b5f5efc0714869bc103a0bca97d4b553166
Josscoder <josscoder@hotmail.com>
2025-07-13 12:09:37 -0500

    se implemento la pantalla de riego por gravedad

---
+++ app/src/main/java/com/pumpapp/pumpapp/RiegoPorGravedadActivity.kt
@@ -0,0 +1,27 @@
+package com.pumpapp.pumpapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RiegoPorGravedadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_riego_por_gravedad)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnAtras = findViewById<Button>(R.id.btn_atras2)
+        btnAtras.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
