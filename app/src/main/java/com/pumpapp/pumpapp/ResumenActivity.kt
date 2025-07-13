b735b50c18e7fce7aa9ed91cc34ff3630cf2f3d8
Josscoder <josscoder@hotmail.com>
2025-07-13 13:54:50 -0500

    se añadio la actividad de resumen

---
+++ app/src/main/java/com/pumpapp/pumpapp/ResumenActivity.kt
@@ -0,0 +1,30 @@
+package com.pumpapp.pumpapp
+
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ResumenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_resumen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnInicio = findViewById<Button>(R.id.btn_inicio)
+        btnInicio.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            val pasar = MediaPlayer.create(this, R.raw.kara)
+            pasar.start()
+        }
+    }
+}
