ee12009c9fda4cfa3153b21fa64428c5ed655074
moninki <moninki63@gmail.com>
2025-07-14 22:53:16 -0500

    Actividades completas
    se agregaron las actividades de riego por aspersión y la de goteo, ya seguiria programarlas

---
+++ app/src/main/java/com/pumpapp/pumpapp/RiegoAspersionAspersor.kt
@@ -0,0 +1,20 @@
+package com.pumpapp.pumpapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RiegoAspersionAspersor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_riego_aspersion_aspersor)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
